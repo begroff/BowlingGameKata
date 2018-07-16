900674e9d90250af4b83ecbfd83d2b44323241a3
groffTECH <brett@grofftech.net>
2018-07-16 07:32:18 -0400

    Finish tests for bowling game

--- BowlingGameTests/BowlingGame.Tests.cs
+++ BowlingGameTests/BowlingGame.Tests.cs
@@ -9,7 +9,8 @@ namespace BowlingGameTests
         private Game g;
 
         [TestInitialize]
-        public void TestSetup() {
+        public void TestSetup()
+        {
             g = new Game();
         }
 
@@ -28,9 +29,53 @@ namespace BowlingGameTests
         }
 
         [TestMethod]
-        public void Test_AllOnes() {
+        public void Test_AllOnes()
+        {
             rollMany(20, 1);
             Assert.AreEqual(20, g.score());
+        }
+
+        [TestMethod]
+        public void Test_OneSpare()
+        {
+            rollSpare();
+            g.roll(3);
+            g.roll(5);
+            rollMany(17, 0);
+
+            Assert.AreEqual(21, g.score());
+        }
+
+        [TestMethod]
+        public void Test_OneStrike()
+        {
+            rollStrike();
+            g.roll(3);
+            g.roll(4);
+            rollMany(16, 0);
+
+            Assert.AreEqual(24, g.score());
+        }
+
+        [TestMethod]
+        public void Test_PerfectGame()
+        {
+            rollMany(12, 10);
+
+            Assert.AreEqual(300, g.score());
+
+
+        }
+
+        private void rollSpare()
+        {
+            g.roll(5);
+            g.roll(5);
+        }
+
+        private void rollStrike()
+        {
+            g.roll(10);
         }
     }
 }
